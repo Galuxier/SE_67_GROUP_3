461f1298395fefbb23a0c486a04e2553a78a293b
Galuxier <galucier2004@gmail.com>
2025-02-24 21:21:49 +0700

    pages structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
 import Home from './pages/home';
-import RegisterForm from './pages/register';
+import CourseHome from './pages/courses/CourseHome';
+import GymHome from "./pages/gyms/GymHome";
+import EventHome from "./pages/events/EventHome";
+import ShopHome from "./pages/shops/ShopHome";
+import UserProfile from "./pages/users/UserProfile";
 
-
-function App() {
+const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route index element={<CourseHome />} />
+          <Route path="gym" element={<GymHome />} />
+          <Route path="event" element={<EventHome />} />
+          <Route path="shop" element={<ShopHome />} />
+          <Route path="profile" element={<UserProfile />} />
+        </Route>
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
